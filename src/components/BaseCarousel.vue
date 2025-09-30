71b234180bc22e1e7edac3544b256c626bd898a0
Kyunghwa oh <kyunghwaoh@Kyunghwas-MacBook-Air.local>
2025-09-30 09:18:30 +0200

    initial commit - add home page with base dropdown and carousel components

---
+++ src/components/BaseCarousel.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="carousel">
+    <h2>{{ header }}</h2>
+    <img v-for="slide in slides" :src="slide.image?.medium" :alt="slide.alt" :key="slide.id" />
+  </section>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+export interface Slide {
+  id: number
+  image?: {
+    medium: string
+    original: string
+  }
+  alt?: string
+}
+
+const props = defineProps<{
+  slides: Slide[]
+  perPage?: number
+  header: string
+}>()
+
+onMounted(() => {
+  console.log(props.slides)
+})
+</script>
---
+++ src/components/BaseDropdown.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface BaseDropdownProps {
+  id: string
+  label: string
+  options: readonly string[]
+  name?: string
+  placeholder?: string
+}
+
+defineProps<BaseDropdownProps>()
+
+const selected = defineModel<string>()
+</script>
+
+<template>
+  <label :for="id">{{ label }}: </label>
+  <select v-model="selected" :name="name" :id="id">
+    <option value="" selected>{{ placeholder }}</option>
+    <option v-for="option in options" :key="option">{{ option }}</option>
+  </select>
+</template>
